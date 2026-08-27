a246439c18e8c7b01c3a76c43c475f2a1b266b02
Franco <parra.efe@gmail.com>
2026-08-26 22:58:12 -0400

    delete(test): remove unused test page component

--- src/routes/test/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import { tokenize } from '$lib/correction/blame';
-
-	const text = 'This is a     tokenized message. I hope you can understand!';
-	const tokenizedText = tokenize(text);
-
-	$inspect(tokenizedText);
-</script>
